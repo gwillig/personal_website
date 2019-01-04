82de8001c9f4d3cb0fbb454a2b3ba29ddf812eaa
gustav <gustavwillig@gmail.com>
2019-01-04 16:21:53 +0100

    improve draw

--- blog/static/blog/js/draw.js
+++ blog/static/blog/js/draw.js
@@ -130,14 +130,6 @@
     }
 
     //=====================Upload image
-
-            window.ajaxSuccess = function () {
-            response = JSON.parse(this.responseText);
-          console.log("ajaxSuccess", typeof this.responseText);
-          document.getElementById('uploaded').setAttribute("src", response["secure_url"]);
-          document.getElementById('results').innerText = this.responseText;
-        }
-
         window.AJAXSubmit = function (formElement) {
           console.log("starting AJAXSubmit");
           if (!formElement.action) { return; }
@@ -150,10 +142,14 @@
               if (input.files && input.files[0]) {
                 var reader = new FileReader();
 
+
                 reader.onload = function(e) {
-                  $('#blah').attr('src', e.target.result);
+                  $('#img_review').attr('src', e.target.result);
                 }
 
                 reader.readAsDataURL(input.files[0]);
               }
-            }
+            };
+    function predication(){
+        document.getElementById('results').innerText = "Picture and drawing successfully comitted. Please wait 10 minutes."
+        }
