518a5ffbe340636988c24153e81ca1fb551e4d18
gwillig <gustavwillig@gmail.com>
2020-08-17 21:42:10 +0000

    MOD| improved style

---
+++ homepage/static/js/homepage.js
@@ -0,0 +1,20 @@
+ var typed2 = new Typed('#greeting', {
+    strings: ['<h2 class=" welcome text-primary font-weight-normal mb-0">Hi, I am</h2><h1 class=" text-xl mb-0">Gustav <span class="text-primary">Willig</span></h1><span id="job" class="h4 font-weight-normal mb-5">Front-End Developer ',],
+    typeSpeed: 100,
+    backSpeed: 0,
+    fadeOut: true,
+    cursorChar: '|',
+    onComplete: function(self) {  var typed2 = new Typed('#job', {
+    strings: ["AI Engineer", "Full-Stack Developer",'AI Full-Stack Developer',],
+    typeSpeed: 100,
+    backSpeed: 0,
+    fadeOut: true,
+    cursorChar: '|'
+  });
+
+
+    },
+  });
+
+
+
