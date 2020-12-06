4e449224483c4521de073b5ecd1d928fa719439c
gwillig <gustavwillig@gmail.com>
2020-12-06 08:30:31 +0100

    MOD| settings.py, env_json| Created and removed SECRET for production

--- homepage/admin.py
+++ homepage/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from homepage.models import ControleWebpage
+
+admin.site.register(ControleWebpage)
--- homepage/models.py
+++ homepage/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
+class ControleWebpage(models.Model):
+    raw_cmd =  models.TextField()
+    raw_history = models.TextField()
